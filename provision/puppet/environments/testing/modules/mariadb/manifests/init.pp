10523d6c638cdb3b538554dc45b585c5913c2626
Ivan Zinovyev <vanyazin@gmail.com>
2016-01-24 02:42:20 +0300

    MariaDB packages

--- provision/puppet/environments/testing/modules/mariadb/manifests/init.pp
+++ provision/puppet/environments/testing/modules/mariadb/manifests/init.pp
@@ -21,10 +21,58 @@ class mariadb {
     require => Package[$mysql],
   }
 
+  # Backup old configuration (for my.cnf)
+  file { '/etc/mysql/my.cnf.bkp':
+    ensure  => 'file',
+    source  => '/etc/mysql/my.cnf',
+    replace => false,
+  }
+
+  # Backup old configuration (for mariadb.cnf)
+  file { '/etc/mysql/conf.d/mariadb.cnf.bkp':
+    ensure  => 'file',
+    source  => '/etc/mysql/conf.d/mariadb.cnf',
+    replace => false,
+  }
+
+  # Create new configuration
+  file { '/etc/mysql/conf.d/mariadb.cnf':
+    ensure  => 'file',
+    content => template('mariadb/mariadb.cnf.erb'),
+    notify  => Service['mysql.service'],
+    require => File['/etc/mysql/conf.d/mariadb.cnf.bkp'],
+  }
+
+  # Drop anonimous user
+  exec { 'mysql_remove_anonimous_user':
+    command => "mysql -e 'DELETE FROM mysql.user WHERE User=\"\";'"
+  }
+
+  # Remove remote root
+  exec { 'mysql_remove_remote_root':
+    command => "mysql -e 'DELETE FROM mysql.user WHERE User=\"root\" AND Host NOT IN (\"localhost\", \"127.0.0.1\", \"::1\");'"
+  }
+
+  # Remove test DB
+  exec { 'mysql_remove_test_db':
+    command => "mysql -e 'DROP DATABASE IF EXISTS test;'"
+  }
+
+  # Flush privileges
+  exec { 'mysql_enable_changes':
+    command => 'mysql -e "FLUSH PRIVILEGES;"',
+    require => [
+      Exec['mysql_remove_anonimous_user'],
+      Exec['mysql_remove_remote_root'],
+      Exec['mysql_remove_test_db'],
+    ],
+    notify => Service['mysql.service'],
+  }
+
   # Enable mariadb service
   service { 'mysql.service':
-    ensure => 'running',
-    enable => true,
+    ensure  => 'running',
+    enable  => true,
     require => Package[$mariadb],
   }
 }
